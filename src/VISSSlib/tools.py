a05b7edd9f24f77831a201dcfcbd666047dbcdf6
Maximilian Maahn <maximilian.maahn@colorado.edu>
2024-10-18 13:50:58 +0200

    reorganized workers

--- src/VISSSlib/tools.py
+++ src/VISSSlib/tools.py
@@ -3,6 +3,7 @@
 import datetime
 import io
 import logging
+import multiprocessing
 import os
 import shutil
 import socket
@@ -1065,3 +1066,90 @@ def runCommandInQueue(IN, stdout=subprocess.DEVNULL):
             pass
 
     return success
+
+
+class TaskQueuePatched(taskqueue.TaskQueue):
+    def is_empty_wait(self):
+        # first delay everything if there are no jobs
+        for i in range(2):
+            if not self.is_empty():
+                break
+            print(f"waiting for jobs... {i}", flush=True)
+            time.sleep(waitTime)
+
+        # if there are really no jobs, nothing to do
+        if self.is_empty():
+            return True
+
+        # if there are jobs, check for killwitch file
+        if os.path.isfile("VISSS_KILLSWITCH"):
+            print(f"{ii}, found file VISSS_KILLSWITCH, stopping", flush=True)
+            return True
+
+        # if tehre are jobsm check for memory and wait otherwise
+        while True:
+            if psutil.virtual_memory().percent < 95:
+                break
+            print(f"waiting for available memory...", flush=True)
+            time.sleep(waitTime)
+        return self.is_empty()
+
+
+def worker1(queue, ww=0, status=None, waitTime=5):
+    print(f"starting worker {ww} for {queue}", flush=True)
+    time.sleep(ww / 5.0)  # to avoid race conditions
+    tq = TaskQueuePatched(f"fq://{queue}")
+    out = None
+    while True:
+        if not tq.is_empty():
+            if status is not None:
+                status[ww] = 1
+            try:
+                out = tq.poll(
+                    verbose=True,
+                    tally=True,
+                    stop_fn=tq.is_empty_wait,
+                    lease_seconds=2,
+                    backoff_exceptions=[BlockingIOError],
+                )
+            except:
+                pass
+            finally:
+                if status is not None:
+                    status[ww] = 0
+        else:
+            print(f"worker {ww} queueu {queue} empty", flush=True)
+        if status is not None:
+            if np.all([ss == 0 for ss in status]):
+                print(
+                    f"do not restart worker {ww} because all empty {[status[i] for i in range(len(status))]}",
+                    flush=True,
+                )
+                break
+            summary = [status[i] for i in range(len(status))]
+        else:
+            summary = ""
+        print(
+            f"restart worker {ww} {summary}",
+            flush=True,
+        )
+        time.sleep(waitTime)
+
+    return out
+
+
+def workers(queue, nJobs=os.cpu_count(), waitTime=60):
+    # for communication between subprocesses
+    print(f"starting {nJobs} workers")
+    status = multiprocessing.Array("i", [0] * nJobs)
+    for ww in range(nJobs):
+        x = multiprocessing.Process(
+            target=worker1,
+            args=(queue,),
+            kwargs={
+                "ww": ww,
+                "status": status,
+                "waitTime": waitTime,
+            },
+        )
+        x.start()

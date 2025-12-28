5c822dfce6f02ea604e3c359389ac35ddb590fb9
Maximilian Maahn <maximilian-maahn@uni-leipzig.de>
2025-12-28 21:02:48 +0100

    Update VISSSlib_slurm.sh

--- scripts/VISSSlib_slurm.sh
+++ scripts/VISSSlib_slurm.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
 
-# sample script to process VISSS data on slurm cluster
-
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --partition=pq2
 
-#$1 settings file
-#$2 nDays to look back 
-
-cd ~/slurm
-
-# all products but metarotation
-echo "python -m VISSSlib scripts.loopCreateBatch  $1 $2 1"
-python -m VISSSlib scripts.loopCreateBatch  $1 $2 1
-
-# metarotation
-echo "python3 -m VISSSlib scripts.loopCreateMetaRotation  $1 $2 1"
-python3 -m VISSSlib scripts.loopCreateMetaRotation  $1 $2 1
 
 
-
-
-
-
-
+# everything but metarotation
+echo "python -m VISSSlib products.submitAll /projekt1/ag_maahn/VISSS_config/$1 $2 /projekt6/ag_maahn/visss_task_queue_1.2"
+python -m VISSSlib products.submitAll /projekt1/ag_maahn/VISSS_config/$1 $2 /projekt6/ag_maahn/visss_task_queue_1.2
